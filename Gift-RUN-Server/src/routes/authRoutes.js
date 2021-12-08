2532be60505311f46c433b8e524860aa3f8ea4e2
“Ron <wheresronald0@icloud.com>
2021-12-08 15:46:52 -0500

    configed JWT using the _user.id from Mongo, and confirm via Postman that a toke is being returned

--- Gift-RUN-Server/src/routes/authRoutes.js
+++ Gift-RUN-Server/src/routes/authRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 const User = mongoose.model("User");
 
 const router = express.Router();
@@ -8,10 +9,16 @@ router.post("/signup", async (req, res) => {
   //console.log(req.body);
   const { email, password } = req.body;
 
-  const user = new User({ email, password });
-  await user.save();
-
-  res.send("You made a post request");
+  try {
+    //if the same use exist Mongo will catch it and throw an error
+    const user = new User({ email, password });
+    await user.save();
+    //get JWT
+    const token = jwt.sign({ userId: user._id }, "wheres_Ronald0s_Key");
+    res.send({ token: token });
+  } catch (err) {
+    return res.status(422).send(err.message);
+  }
 });
 
 module.exports = router;
